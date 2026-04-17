8df001e6b6647d313e94c38faf2e2dd090946b16
Tanmayee <n.tanmayee@gmail.com>
2026-04-17 16:15:02 +0100

    updated linkage and fcluster load

--- tangerine/visualise/data_loader.py
+++ tangerine/visualise/data_loader.py
@@ -1,11 +1,17 @@
-import networkx as nx
-import pandas as pd
 from os.path import join
+
+import networkx as nx
 import numpy as np
+import pandas as pd
+from scipy.cluster.hierarchy import linkage
 
 
 class DataLoader(object):
-    def __init__(self, timepoints, base_path, ) -> None:
+    def __init__(
+        self,
+        timepoints,
+        base_path,
+    ) -> None:
         self.timepoints = timepoints
         self.base_path = base_path
         # load data
@@ -14,8 +20,20 @@ class DataLoader(object):
         self.tf_list = self.get_tf_list()
         self.tf_louvain = self.load_tf_louvain()
 
+        self.linkage_matrices = {}
+        self.baseline_k = {}
+        for t in self.timepoints:
+            corr_df = self.tf_correlation_dfs[t]
+
+            # Pass the 2D correlation array directly so SciPy uses Euclidean distance
+            X = corr_df.fillna(0.0).values
+            self.linkage_matrices[t] = linkage(X, method="ward")
+
+            # Count the baseline number of clusters
+            self.baseline_k[t] = self.tf_louvain[t].nunique()
+
     def load_tf_louvain(self):
-        return pd.read_csv(join(self.base_path, 'louvain_tf.csv'), index_col=0)
+        return pd.read_csv(join(self.base_path, "louvain_tf.csv"), index_col=0)
 
     def get_tf_list(self):
         time = self.timepoints[0]
@@ -24,68 +42,76 @@ class DataLoader(object):
     def load_tf_correlations(self):
         corr_dfs = {}
         for time in self.timepoints:
-            corr_dfs[time] = pd.read_csv(join(self.base_path, f'tf_tf_{time}.csv'), index_col=0)
+            corr_dfs[time] = pd.read_csv(
+                join(self.base_path, f"tf_tf_{time}.csv"), index_col=0
+            )
         return corr_dfs
-    
+
     def get_parcat_dimensions(self):
-        self.dimensions = [dict(values=self.tf_louvain[label], label=label) for label in self.timepoints]
+        self.dimensions = [
+            dict(values=self.tf_louvain[label], label=label)
+            for label in self.timepoints
+        ]
         return self.dimensions
 
     def get_tf_corr_df(self, gene, tf_list):
         # make dataframe for this tf
         avg_corr = pd.DataFrame(index=tf_list)
         for time in self.timepoints:
-            avg_corr[time] = self.tf_correlation_dfs[time].loc[gene, tf_list]       
+            avg_corr[time] = self.tf_correlation_dfs[time].loc[gene, tf_list]
         return avg_corr
-    
+
     def get_tf_corr_basic(self, time):
         return self.tf_correlation_dfs[time], list(self.tf_correlation_dfs[time].index)
 
     def load_networks(self):
         networks = {}
         for time in self.timepoints:
-            networks[time] = nx.read_gml(join(self.base_path, f'network_{time}.gml.gz'))
+            networks[time] = nx.read_gml(join(self.base_path, f"network_{time}.gml.gz"))
         return networks
 
     def get_out_edges_dataframe(self, tf_name, attr_name, threshold=0.1):
-        index = [v for _,v in self.networks[self.timepoints[0]].out_edges(tf_name)]
+        index = [v for _, v in self.networks[self.timepoints[0]].out_edges(tf_name)]
         data_df = pd.DataFrame(index=index)
 
         for time in self.timepoints:
             values = []
             for gene in index:
-                values.append(self.networks[time].get_edge_data(tf_name, gene)[attr_name])
+                values.append(
+                    self.networks[time].get_edge_data(tf_name, gene)[attr_name]
+                )
             data_df[time] = values
-        data_df['avg'] = data_df.mean(axis=1)
-        
+        data_df["avg"] = data_df.mean(axis=1)
+
         return data_df[np.abs(data_df.avg) > threshold]
 
     def get_in_edges_dataframe(self, gene_name, attr_name, threshold=0.05):
-        index = [u for u,_ in self.networks[self.timepoints[0]].in_edges(gene_name)]
+        index = [u for u, _ in self.networks[self.timepoints[0]].in_edges(gene_name)]
         data_df = pd.DataFrame(index=index)
-        
+
         for time in self.timepoints:
             values = []
             for tf_name in index:
-                values.append(self.networks[time].get_edge_data(tf_name, gene_name)[attr_name])
-            data_df[time] = values 
-        data_df['avg'] = data_df.mean(axis=1)
-        
+                values.append(
+                    self.networks[time].get_edge_data(tf_name, gene_name)[attr_name]
+                )
+            data_df[time] = values
+        data_df["avg"] = data_df.mean(axis=1)
+
         return data_df[np.abs(data_df.avg) > threshold]
-    
+
     def get_metacell_expression(self, timepoint, tf_list):
         import os
+
         import pandas as pd
-        
+
         file_path = os.path.join(self.base_path, f"metacells_{timepoint}.parquet")
-        
+
         if not os.path.exists(file_path):
             return pd.DataFrame()
-            
+
         try:
             return pd.read_parquet(file_path, columns=tf_list)
         except ValueError:
             # Triggers if the TFs are somehow missing from the saved parquet file
             return pd.DataFrame()
-
-    
